1cc3bf59c20530002e62d137358c88883ca75bd2
Sukhorukov Dmitry <DimaJTX@mail.ru>
2022-08-12 19:22:41 +0700

    Добавьте файлы проекта.

---
+++ DragandDrop/Data/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragandDrop.Data
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public TaskStatuses Status { get; set; }
+        public string Detail { get; set; }
+        public  DateTime UpDatedTime { get; set; }
+    }
+
+    public enum TaskStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
